9ef5250d4e18e3ff33f52db2d3de430525cc2fec
HarryFray <nick.fray@merlinlabs.com>
2019-09-13 14:04:21 -0500

    feat(site): created cloudinary test app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Image, Transformation, CloudinaryContext } from "cloudinary-react";
+import "./App.css";
+import styled from "styled-components";
+
+const Images = styled.div`
+  display: flex;
+`;
 
 function App() {
+  const [isLoaded, setLoaded] = useState(false);
+  console.log(process.env.PARTY);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <CloudinaryContext cloudName=''>
+        <Images>
+          {isLoaded ? null : (
+            <Image publicId='land.jpg'>
+              <Transformation effect='blur:1000' quality='1' />
+              <Transformation effect='cartoonify' />
+            </Image>
+          )}
+          <Image onLoad={() => setLoaded(true)} publicId='land.jpg'></Image>
+        </Images>
+      </CloudinaryContext>
     </div>
   );
 }
